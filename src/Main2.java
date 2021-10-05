4a1b176976fbde02e8ea8c2230297937d741440e
Maria Kovalova <123@spar.pl>
2021-10-05 22:06:40 +0200

    Students

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+public class Main2 {
+
+    public static void main(String[] args) {
+        Student student = new Student("Orlov O.P.", "26.03.95", 725936594, 15 );
+
+        System.out.println(student.getFIO() + " " + student.getDateOfBirth() + " " + student.getPhone() +" " + student.getNumberOfGroup());
+
+
+
+
+    }
+}
