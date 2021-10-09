f9562b998c7e82f0cb670ed519a24ceba9e3ea10
Maria Kovalova <123@spar.pl>
2021-10-09 11:33:12 +0200

    added src

--- src/JavaBasic/Passport/Main.java
+++ src/JavaBasic/Passport/Main.java
@@ -4,6 +4,6 @@ public class Main {
         public static void main(String[] args) {
             Passport passport = new Passport("Kovalova M.A.");
             ForeignPassport foreignPassport = new ForeignPassport("Kovalova M.A.","vise kat.D" , "FK123456");
-            System.out.println( foreignPassport.getVise() + " " + foreignPassport.getPasNumber());
+            System.out.println(foreignPassport.getVise() + " " + foreignPassport.getPasNumber());
         }
     }
